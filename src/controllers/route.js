23fd8b1e287716de3a0abbdbbe4f2a2e45d416c2
Paul Hanna <paulhanna1933@gmail.com>
2017-08-23 13:00:21 +0300

    create router - connect router to home page relates #27

---
+++ src/controllers/route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const home = require('../views/home');
+
+router.get('/', (req, res) => {
+   res.render('home', { activePage: {home: true}});
+});
+
+// This is going to be our user profile route
+
+// router.get('/', (req, res) => {
+//    res.render('home', { activePage: {home: true}});
+// });
+
+
+module.exports = router;
