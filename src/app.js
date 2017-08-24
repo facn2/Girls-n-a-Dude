beeb66fb62bed5af16e54c8cfabdcfb986759ae1
heathercoraje <yhk7830@hotmail.com>
2017-08-24 13:55:13 +0300

    fix bug in rendering html from server and build db relates #51

--- src/app.js
+++ src/app.js
@@ -12,14 +12,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine(
-   'hbs',
-   exphbs({
-      extname: 'hbs',
-      layoutsDir: path.join(__dirname, 'views', 'partials/layouts'),
-      partialsDir: path.join(__dirname, 'views', 'partials'),
-      defaultLayout: 'main',
-      helpers: helpers
-   }));
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+    helpers: helpers
+  }));
 
 
 
--- src/controllers/router.js
+++ src/controllers/router.js
@@ -3,35 +3,19 @@ const path = require('path');
 const router = express.Router();
 
 const getProfiles = require('../queries/getProfiles.js');
-const home = require('../views/home');
-router.get('/', (req, res) => {
+router.get('/', (request, response) => {
   getProfiles((err, data) => {
-    console.log("res", data);
     if (err) {
-  		res.send('<h1>Sorry, there was a problem getting the data</h1>');
-  	  console.log("err", err);
-  	} else {
-  		const dataObj = JSON.stringify(data);
-      res.render('home', {members: dataObj});
-  	}
+      res.send('<h1>Sorry, there was a problem getting the data</h1>');
+      console.log("err", err);
+    } else {
+      console.log(data[0].image);
+      return response.render('home', {
+        data
+      });
+    }
   })
 });
-
-// user profile route
-
-// router.get('/profile', (req, res) => {
-//
-// });
-
-
-
-// This is going to be our user profile route
-
-// router.get('/', (req, res) => {
-//    res.render('home', { activePage: {home: true}});
-// });
-
-
 
 
 module.exports = router;
